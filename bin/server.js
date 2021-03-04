be3268ffb3314ad507757d2b46e1b8aa0ce1f157
TGA Sistemas de Informatica <68694351+tgasistemas@users.noreply.github.com>
2021-03-04 15:19:54 -0400

    Add files via upload
    up

---
+++ bin/server.js
@@ -0,0 +1,62 @@
+const app = require('../src/app');
+const debug = require('debug')('balta:server');
+const http = require('http');
+const teste = require('../src/controllers/license-controller')
+
+const port = normalizePort(process.env.PORTAPI || '3000');
+// setInterval(file.writeImage, 300000);
+// setInterval(file.writeImageGroup, 185090);
+// teste.getLicense2();
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+console.log('API rodando na porta ' + port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
