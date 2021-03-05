e927a33e53bdafa256c25aaae6abab86772de860
pablo.freitas28@gmail.com <Rego@1234>
2021-03-05 09:05:30 -0400

    up

--- src/connection/firebird.js
+++ src/connection/firebird.js
@@ -1,9 +1,11 @@
 'use strict';
 require('dotenv/config');
+console.log('user '+process.env.USER);
+console.log('pass '+process.env.PASS);
 exports.options = {
     host: process.env.HOST,
     port: process.env.PORTFB,
-    database: process.env.DATABASE, //"C:/TGA/Dados/TGA.FDB", C:\\TGA\\Dados\\CONTROLE\\NODE.FDB
+    database: "E:\\Base Clientes\\Supreloja\\TGA.FDB",//process.env.DATABASE, //"C:/TGA/Dados/TGA.FDB", C:\\TGA\\Dados\\CONTROLE\\NODE.FDB
     user: process.env.USER,
     password: process.env.PASS,
     lowercase_keys: false, // set to true to lowercase keys
