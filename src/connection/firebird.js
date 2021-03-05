3cb01c47d5a699eed9a2f4b86f52c99a128047b6
pablo.freitas28@gmail.com <Rego@1234>
2021-03-05 09:08:20 -0400

    up

--- src/connection/firebird.js
+++ src/connection/firebird.js
@@ -6,7 +6,7 @@ exports.options = {
     host: process.env.HOST,
     port: process.env.PORTFB,
     database: "E:\\Base Clientes\\Supreloja\\TGA.FDB",//process.env.DATABASE, //"C:/TGA/Dados/TGA.FDB", C:\\TGA\\Dados\\CONTROLE\\NODE.FDB
-    user: process.env.USER,
+    user: 'SYSDBA',// process.env.USER,
     password: process.env.PASS,
     lowercase_keys: false, // set to true to lowercase keys
     role: null,            // default
